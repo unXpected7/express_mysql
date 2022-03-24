bd909381b2bd92e4e91f76f1c9c8d90ea208912f
unXpected7 <ilyasyusuf002@gmail.com>
2022-03-24 09:42:10 +0700

    config to database mysql2

---
+++ config/products.js
@@ -0,0 +1,21 @@
+const {Sequelize} = require ('sequelize');
+
+const sequelize = new Sequelize({
+    database: 'merncruds',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    dialect: 'mysql'
+});
+
+(async () =>{
+    try{
+        await sequelize.authenticate()
+        console.log('Connection has been established succesfully')
+    }
+    catch (error){
+        console.error('Unable to connect to the database:', error)
+    }
+})();
+
+module.exports = sequelize
