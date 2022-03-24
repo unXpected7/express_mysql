da493a3dfe585754afebe8b55dcd1639782c57d0
unXpected7 <ilyasyusuf002@gmail.com>
2022-03-24 09:41:14 +0700

    makes rest api with sequelize

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const routes = require("./routes/router");
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.use("/product", routes);
+
+app.listen(PORT, async () => {
+    console.log('success in port 8000')
+});
